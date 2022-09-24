f400a841daa10ce313c8a86f3e82501d5faf696e
Santosh Reddy <112513931+Santhoshreddy9278@users.noreply.github.com>
2022-09-24 10:20:36 +0530

    day 5-8th program

---
+++ 48.matrix multiplication.py
@@ -0,0 +1,24 @@
+n = 3
+m = 3
+def countSquareMatrices(a, N, M):
+	
+	count = 0
+
+	for i in range(1, N):
+		for j in range(1, M):
+			if (a[i][j] == 0):
+				continue
+
+			
+			a[i][j] = min([a[i - 1][j],
+					a[i][j - 1], a[i - 1][j - 1]])+1
+	for i in range(N):
+		for j in range(M):
+			count += a[i][j]
+
+	return count
+arr = [ [ 1, 0, 1],
+	[ 1, 1, 0 ],
+	[ 1, 1, 0 ] ]
+
+print(countSquareMatrices(arr, n, m))
